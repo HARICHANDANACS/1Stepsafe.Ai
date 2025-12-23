dc1d2283aa06a47bcffaf4164dc1e6bc702399f0
C S HARI CHANDANA <harichandana349@gmail.com>
2025-12-23 12:09:47 +0000

    error pahe

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -1,13 +1,19 @@
 import Link from "next/link";
 import { StepSafeLogo } from "@/components/icons";
+import { UserNav } from "./user-nav";
 
 export function Header() {
   return (
     <header className="sticky top-0 z-50 w-full border-b bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
       <div className="container flex h-16 items-center">
-        <Link href="/">
-          <StepSafeLogo />
-        </Link>
+        <div className="mr-4 hidden md:flex">
+          <Link href="/" className="mr-6 flex items-center space-x-2">
+            <StepSafeLogo />
+          </Link>
+        </div>
+        <div className="flex flex-1 items-center justify-end space-x-4">
+          <UserNav />
+        </div>
       </div>
     </header>
   );
