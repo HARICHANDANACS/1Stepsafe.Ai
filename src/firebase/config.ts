43b87b7840fc1b03e58d41c5edf1aa5b6fbad70c
C S HARI CHANDANA <harichandana349@gmail.com>
2026-01-01 12:10:52 +0000

    Google API Key Open GitHub detected a secret 56 seconds ago Publicly lea

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,8 +1,7 @@
 export const firebaseConfig = {
-  "projectId": "studio-9761139617-7ba32",
-  "appId": "1:580921450150:web:97be953a81c32017b01e67",
-  "apiKey": "AIzaSyA7pwPDhklhHnMVuwawqX3-8jzUEuS0RQg",
-  "authDomain": "studio-9761139617-7ba32.firebaseapp.com",
-  "measurementId": "",
-  "messagingSenderId": "580921450150"
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
 };
